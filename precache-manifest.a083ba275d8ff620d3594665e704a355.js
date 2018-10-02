fa68deaf558ab52c69399386b09088388efee39f
Sigurður Snær Eiríksson <sse1987@gmail.com>
2018-10-02 12:09:53 +0000

    Updates

---
+++ precache-manifest.a083ba275d8ff620d3594665e704a355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84cfcff3eb4b6ebdf71d",
+    "url": "/listlist/static/js/runtime~main.84cfcff3.js"
+  },
+  {
+    "revision": "3834505cba5a22407b7f",
+    "url": "/listlist/static/js/main.3834505c.chunk.js"
+  },
+  {
+    "revision": "e54cb8d1a06d38915d89",
+    "url": "/listlist/static/js/1.e54cb8d1.chunk.js"
+  },
+  {
+    "revision": "3834505cba5a22407b7f",
+    "url": "/listlist/static/css/main.49e8ebd1.chunk.css"
+  },
+  {
+    "revision": "5810850deaf444afe82678351884c5d9",
+    "url": "/listlist/index.html"
+  }
+];
