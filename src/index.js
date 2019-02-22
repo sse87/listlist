4cc9a7e8508489256629690c5465b2496d131a7c
Sigurður Snær Eiríksson <sse1987@gmail.com>
2019-02-22 16:30:13 +0000

    Added service worker code from cra2 that will able offline and other pwa features

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './index.css'
 
 import App from './App'
 
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker'
 
 import packageJson from '../package.json'
 window.appVersion = packageJson.version
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register()
