01b4c51f93ae28931f8b9d50372c88aedc503dfd
Sigurður Snær Eiríksson <sse1987@gmail.com>
2019-07-14 22:24:10 +0000

    Updates

---
+++ precache-manifest.fbd0feab4e3428c36c2dd5dbe55d34ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cfe83d1a855504020fbe4c26d2b496",
+    "url": "/listlist/index.html"
+  },
+  {
+    "revision": "7087b40239dda6e21a56",
+    "url": "/listlist/static/css/main.db80cd4b.chunk.css"
+  },
+  {
+    "revision": "7a5bdf78da7e8dc80e32",
+    "url": "/listlist/static/js/2.7362f41d.chunk.js"
+  },
+  {
+    "revision": "7087b40239dda6e21a56",
+    "url": "/listlist/static/js/main.dc667e4f.chunk.js"
+  },
+  {
+    "revision": "71837b7c123e14917283",
+    "url": "/listlist/static/js/runtime~main.19a1034a.js"
+  }
+]);
